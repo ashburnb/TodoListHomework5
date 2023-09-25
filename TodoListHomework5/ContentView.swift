d808128842adf7935c30ba72b45beff620e67acd
Brad Ashburn <bradleyashburn@icloud.com>
2023-09-24 17:33:44 -1000

    Initial Commit

---
+++ TodoListHomework5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoListHomework5
+//
+//  Created by Brad Ashburn on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoListHomework5/TodoListHomework5App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListHomework5App.swift
+//  TodoListHomework5
+//
+//  Created by Brad Ashburn on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListHomework5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
