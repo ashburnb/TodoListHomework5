aac52fbaa3b754749c8805f0048888baa447b1ce
Brad Ashburn <bradleyashburn@icloud.com>
2023-09-27 13:26:30 -1000

    Extracted more views

---
+++ TodoListHomework5/Views/AddButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  AddButtonView.swift
+//  TodoListHomework5
+//
+//  Created by Brad Ashburn on 9/27/23.
+//
+
+import SwiftUI
+
+struct AddButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddButtonView()
+}
---
+++ TodoListHomework5/Views/TaskListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListRowView.swift
+//  TodoListHomework5
+//
+//  Created by Brad Ashburn on 9/27/23.
+//
+
+import SwiftUI
+
+struct TaskListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListRowView()
+}
